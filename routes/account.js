467fd96f05ac26a3cceeaaedb2984d3f83e691b7
Paul Martinetti <paul@digscitech.com>
2019-03-24 13:46:12 -0400

    I coded a remove item route

--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const Item = require('../models/Item')
 
 router.get('/', (req, res, next) => {
     const user = req.user
-    
+
     // reload home / login / register 
     if (user == null) {
         res.redirect('/')
@@ -18,17 +18,26 @@ router.get('/', (req, res, next) => {
 
         if (err) return next(err)
 
-        const data = {
-            user: user,
-            itemsA: itemsA
-        }
-        // hjs connection
-        res.render('account', data)
+        // mongoose w filter interested
+        // even tho interested=Array, can pass param like this
+        Item.find({ interested: user._id }, (err, interestedItemsA) => {
+            if (err) return next(err)
+
+            const data = {
+                user: user,
+                itemsA: itemsA,
+                interestedItemsA: interestedItemsA
+            }
+            // hjs connection
+            res.render('account', data)
+        })
+
+
     })
 })
 
-// add item route with id
-router.get('/additem/:itemid',(req, res, next)=>{
+// router, endpoint - add item route with id
+router.get('/additem/:itemid', (req, res, next) => {
 
     // required user is logged in, 
     // I'm assuming express-session always passes user obj in req
@@ -40,21 +49,44 @@ router.get('/additem/:itemid',(req, res, next)=>{
     }
 
     // find item
-    Item.findById(req.params.itemid, (err, item)=>{
+    Item.findById(req.params.itemid, (err, item) => {
         if (err) return next(err)
 
         // if item not already interested (array obj of Item), add user to interested
         // -1 is returned on false
-        if (item.interested.indexOf(user._id)==-1){
+        if (item.interested.indexOf(user._id) == -1) {
             item.interested.push(user._id)
             item.save()
-            
-            res.json({
+
+            res.redirect('/account')
+            /* res.json({
                 item:item
-            })
+            }) */
         }
     })
 
+})
+
+router.get('/removeitem/:itemid', (req, res, next) => {
+
+    const user = req.user
+    // reload home / login / register 
+    if (user == null) {
+        res.redirect('/')
+        return
+    }
+
+    // find item, search interested array for user
+    // find item
+    Item.findById(req.params.itemid, (err, item) => {
+        if (err) return next(err)
+
+        // remove user
+        item.interested.splice(item.interested.indexOf(user._id), 1)
+        item.save()
+        // refresh page
+        res.redirect('/account')
+    })
 })
 
 router.get('/logout', (req, res, next) => {
